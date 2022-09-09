cc0421c5bad295035cb9e582a0653ba181099a27
Michael Wyss <michael.wyss@homegate.ch>
2022-09-09 16:13:47 +0200

    Use 3rd party package dbt_utils for building a surrogate key in fct_reviews

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -9,7 +9,8 @@ WITH src_hosts AS (
     SELECT * FROM {{ ref('src_reviews') }}
 )
 SELECT 
-    *
+    {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text', 'review_sentiment'])}} AS review_id
+    , *
 FROM src_hosts
 WHERE review_text IS NOT NULL
 {% if dbt.is_incremental() %}
