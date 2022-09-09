1397cc849d1def6c928b75bdde35d7325b31f12d
Michael Wyss <michael.wyss@homegate.ch>
2022-09-09 15:53:10 +0200

    fix test

--- tests/consistent_created_at.sql
+++ tests/consistent_created_at.sql
@@ -1,4 +1,4 @@
-SELECT *
-FROM ref('dim_listings_cleansed')
-JOIN {{ ref('fct_reviews') }}  USING (listing_id)
-WHERE reviews.review_date < listings.created_at
+SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+INNER JOIN {{ ref('fct_reviews') }} r
+USING (listing_id)
+WHERE l.created_at >= r.review_date
